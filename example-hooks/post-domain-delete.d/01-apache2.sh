2730e03aead1f1052e26d46f2d0ffeb007a6effe
Alex Cartwright <alexc223@googlemail.com>
2010-12-02 17:36:12 +0000

    Improved the quoting of Bash vars

--- example-hooks/post-domain-delete.d/01-apache2.sh
+++ example-hooks/post-domain-delete.d/01-apache2.sh
@@ -5,7 +5,7 @@
 ## Disables and removes the Apache2 vhost
 ##
 
-a2dissite $OZ_DOMAIN_NAME > /dev/null
-unlink /etc/apache2/sites-available/$OZ_DOMAIN_NAME
+a2dissite "$OZ_DOMAIN_NAME" > /dev/null
+unlink "/etc/apache2/sites-available/$OZ_DOMAIN_NAME"
 
 exit 0
